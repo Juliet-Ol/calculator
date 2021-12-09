eebd76cad85423587aa0de9f8323c3afdaced1a6
Juliet-Ol <agutjuliet@gmail.com>
2021-12-09 22:58:41 +0300

    modification of js script

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 var add = function(number1, number2) {
     return number1 + number2;
 };
-var number1 = prompt(3)
-var number2 = prompt(5)
-alert(add(number1, number2));
 
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number"));
+var result = add(number1, number2);
+alert(result);
 
+var subtract = function(number1, number2) {
+    return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number"));
+var result = subtract(number1, number2);
+alert(result);
 
 
   
