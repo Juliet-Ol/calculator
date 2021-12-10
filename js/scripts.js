7430678a5c1a3ac284685a3c7f5139dbdceba41a
Juliet-Ol <agutjuliet@gmail.com>
2021-12-10 19:01:32 +0300

    modify add constants on js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,19 @@
-var add = function(number1, number2) {
-    return number1 + number2;
-};
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number"));
-var result = add(number1, number2);
-alert(result);
-
-var subtract = function(number1, number2) {
-    return number1 + number2;
-};
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number"));
-var result = subtract(number1, number2);
-alert(result);
+class Calculator {
+    constructor(previousOperandTextElement, curentOperandTextElementButton){
+        this.previousOperandTextElement
+        this.currentOperandTextElement
+    }
+     t
+}
 
 
-  
-  
-
-    
+const numberButtons = document.querySelectorAll("[data number]")
+const operationButtons = document.querySelectorAll("[data-opetation]")
+const equalsButton = document.querySelector("[data-equals]")
+const deleteButton = document.querySelector("[data-delete]")
+const allClearButton = document.querySelector("[data-all-clear]")
+const previousOperandTextElementButton = document.querySelector("[data-previous-operand]")
+const curentOperandTextElementButton = document.querySelector("[data-current-operand]")
     
 
 
